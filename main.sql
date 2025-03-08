20443bfa410d4a446534565a64144aa42ec49b91
Artem Goncharuk <artemhoncharuk123@gmail.com>
2025-03-08 14:51:53 +0200

    Project has been edited, but requires fixes.

--- main.sql
+++ main.sql
@@ -73,6 +73,17 @@ create table Archive
 );
 go;
 
+create table LastUnit
+(
+    LastUnitID int identity(1, 1) not null primary key,
+    ProductID int not null foreign key references Products(ProductID),
+    ProductName nvarchar(20) not null,
+    ProductType nvarchar(20) not null,
+    ProductProducer nvarchar(20) not null,
+    SaleDate date not null
+);
+go;
+
 create trigger trg_SalesHistory
 on Sales
 after insert
@@ -94,5 +105,84 @@ begin
     from Products p
     join inserted i on p.ProductID = i.ProductID
     where p.ProductAvailable = 0;
+end;
+go;
+
+create unique index idx_UniqueClient
+    on Clients (ClientSNP, ClientEmail);
+
+create trigger trg_PreventClientDeletion
+    on Clients
+    for delete
+    as
+begin
+    if exists (select 1 from deleted)
+        begin
+            raiserror('Deletion of clients is not allowed.', 16, 1);
+            rollback transaction;
+        end
+end;
+go;
+
+create trigger trg_PreventEmployeeDeletion
+    on Employees
+    for delete
+    as
+begin
+    if exists (select 1 from deleted where EmployeeEmploymentDate < '2015-01-01')
+        begin
+            raiserror('Deletion of employees hired before 2015 is not allowed.', 16, 1);
+            rollback transaction;
+        end
+end;
+go;
+
+create trigger trg_UpdateClientDiscount
+    on Sales
+    after insert
+    as
+begin
+    update Clients
+    set ClientDiscountPercent = 15
+    where ClientID in (
+        select s.ClientID
+        from Sales s
+                 join inserted i on s.ClientID = i.ClientID
+        group by s.ClientID
+        having sum(s.ProductSellingPrice) > 50000
+    );
+end;
+go;
+
+create trigger trg_LastUnitAlert
+    on Sales
+    after insert
+    as
+begin
+    -- Додаємо товар до таблиці LastUnit, якщо залишилася одна одиниця
+    insert into LastUnit (ProductID, ProductName, ProductType, ProductProducer, SaleDate)
+    select p.ProductID, p.ProductName, p.ProductType, p.ProductProducer, i.SaleDate
+    from Products p
+             join inserted i on p.ProductID = i.ProductID
+    where p.ProductAvailable = 1;
+end;
+go;
+
+create trigger trg_PreventSpecificProducer
+    on Products
+    instead of insert
+    as
+begin
+    if exists (select 1 from inserted where ProductProducer = 'Спорт, сонце та штанга')
+        begin
+            raiserror('Додавання товару фірми "Спорт, сонце та штанга" заборонено.', 16, 1);
+            rollback transaction;
+        end
+    else
+        begin
+            insert into Products (ProductName, ProductType, ProductAvailable, ProductCost, ProductSellingPrice, ProductProducer)
+            select ProductName, ProductType, ProductAvailable, ProductCost, ProductSellingPrice, ProductProducer
+            from inserted;
+        end
 end;
 go;
